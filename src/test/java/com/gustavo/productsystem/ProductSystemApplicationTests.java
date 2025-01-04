685350d28f79f86aeb464e892d04aea39b5facfa
gustavoglins <gustavoguimaraes.241025@gmail.com>
2025-01-04 12:56:36 -0300

    .

---
+++ src/test/java/com/gustavo/productsystem/ProductSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.productsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
